37b126847938e8a392fc62195d5f961ac9c3b916
Marcelo Mechi <marcedumechi@gmail.com>
2023-11-30 19:07:01 -0300

    feat: new add to cart service

---
+++ packages/client/src/app/services/cart.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { Food } from '../shared/models/food';
+import { Cart } from '../shared/models/cart';
+import { CartItem } from './../shared/models/cart-item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  private cart: Cart = this.getCartFromLocalStorage();
+  private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart);
+
+  constructor() { }
+
+  addFoodToCart(food: Food): void {
+    let cartItem = this.cart.items.find(item => item.food.id === food.id);
+
+    if (cartItem) {
+      return;
+    }
+
+    this.cart.items.push(new CartItem(food));
+    this.setCartToLocalStorage();
+  }
+
+  removeFoodFromCart(food: Food): void {
+    let cartItem = this.cart.items.find(item => item.food.id === food.id);
+
+    if (!cartItem) {
+      return;
+    }
+
+    this.cart.items = this.cart.items.filter(item => item.food.id !== food.id);
+    this.setCartToLocalStorage();
+  }
+
+  changeQuantity(foodId: string, quantity: number): void {
+    let cartItem = this.cart.items.find(item => item.food.id === foodId);
+
+    if (!cartItem) {
+      return;
+    }
+
+    cartItem.quantity = quantity;
+    cartItem.price = cartItem.food.price * quantity;
+    this.setCartToLocalStorage();
+  }
+
+  clearCart(): void {
+    this.cart = new Cart();
+    this.setCartToLocalStorage();
+  }
+
+  getCartObservable(): Observable<Cart> {
+    return this.cartSubject.asObservable();
+  }
+
+  updateCart(): void {
+    this.cart.totalPrice = this.cart.items.reduce((total, item) => total + item.price, 0);
+    this.cart.totalCount = this.cart.items.reduce((total, item) => total + item.quantity, 0);
+
+    this.cartSubject.next(this.cart);
+  }
+
+  private setCartToLocalStorage(): void {
+    this.cart.totalPrice = this.cart.items.reduce((total, item) => total + item.price, 0);
+    this.cart.totalCount = this.cart.items.reduce((total, item) => total + item.quantity, 0);
+
+    localStorage.setItem('Cart', JSON.stringify(this.cart));
+    this.cartSubject.next(this.cart);
+  }
+
+  private getCartFromLocalStorage(): Cart {
+    let cartJson = localStorage.getItem('Cart');
+    return cartJson ? JSON.parse(cartJson) : new Cart();
+  }
+}
