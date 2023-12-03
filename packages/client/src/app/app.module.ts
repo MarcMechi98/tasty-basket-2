6522d2c9b0c6b15bb5f487bb2e75317b4c505dcc
Marcelo Mechi <marcedumechi@gmail.com>
2023-12-03 17:45:40 -0300

    feat: creates not found component and adds it to the other pages

--- packages/client/src/app/app.module.ts
+++ packages/client/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { TagsComponent } from './components/partials/tags/tags.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { TitleComponent } from './components/partials/title/title.component';
     TagsComponent,
     FoodPageComponent,
     CartPageComponent,
-    TitleComponent
+    TitleComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ packages/client/src/app/components/partials/not-found/not-found.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.scss']
+})
+export class NotFoundComponent {
+  @Input() visible: boolean = false;
+  @Input() message: string = 'Not found :(';
+  @Input() resetLinkText: string = 'Reset';
+  @Input() resetLinkRoute: string = '/';
+}
