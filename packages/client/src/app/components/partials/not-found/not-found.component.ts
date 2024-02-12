608418a3955734885bf82f42b487a5c463d8cdda
Marcelo Mechi <marcedumechi@gmail.com>
2024-02-12 17:48:51 -0300

    fix: not found page #13

--- packages/client/src/app/components/partials/not-found/not-found.component.ts
+++ packages/client/src/app/components/partials/not-found/not-found.component.ts
@@ -1,18 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent implements OnInit{
-  @Input() visible?: boolean = false;
-  @Input() message?: string = 'Not found :(';
-  @Input() resetLinkText?: string = 'Reset';
-
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    console.log('oi')
-  }
+export class NotFoundComponent {
+  @Input() visible?: boolean = true;
+  @Input() message?: string = 'Page not found :(';
+  @Input() resetLinkText?: string = 'Go back to homepage';
 }
