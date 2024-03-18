adf2864a7a5746babf701f9f04e940aae0f6a82b
Marcelo Mechi <marcedumechi@gmail.com>
2024-03-18 17:40:53 -0300

    feat: header styles for tabled and desktop sizes

--- packages/client/src/app/components/partials/header/header.component.ts
+++ packages/client/src/app/components/partials/header/header.component.ts
@@ -1,38 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 import { CartService } from './../../../services/cart.service';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/shared/models/user';
 import { faArrowRightFromBracket, faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  cartQuantity: number = 0;
-  user!: User;
-  faUser = faUser;
-  faArrowRightFromBracket = faArrowRightFromBracket;
-  faShoppingCart = faShoppingCart;
+export class HeaderComponent implements OnInit {
+  public cartQuantity = 0;
+  public user!: User;
+  public isInLoginPage = false;
+  public isScrolledDown = false;
+
+  public faUser = faUser;
+  public faArrowRightFromBracket = faArrowRightFromBracket;
+  public faShoppingCart = faShoppingCart;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolledDown = window.scrollY > 0;
+  }
 
   constructor(
-    cartService: CartService,
-    private userService: UserService
-  ) {
-    cartService.getCartObservable().subscribe(newCart => {
+    private cartService: CartService,
+    private userService: UserService,
+    private router: Router
+
+  ) {}
+
+  ngOnInit(): void {
+    this.cartService.getCartObservable().subscribe(newCart => {
       this.cartQuantity = newCart.totalCount
     });
 
-    userService.user$.subscribe(user => this.user = user);
+    this.userService.user$.subscribe(user => this.user = user);
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isInLoginPage = event.url.includes('login');
+      }
+    });
   }
 
   get isLoggedIn(): boolean {
     return this.user?.token ? true : false;
   }
 
-  logout(): void {
+  public logout(): void {
     this.userService.logout();
   }
 }
