ef00358f762999481bcb2660e474ffea5c866698
Marcelo Mechi <marcedumechi@gmail.com>
2024-04-11 16:03:44 -0300

    feat: improves quantity selector visuals

--- packages/client/src/app/components/pages/cart-page/cart-page.component.ts
+++ packages/client/src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { faCaretRight, faCaretLeft } from '@fortawesome/free-solid-svg-icons';
 
 import { Cart } from 'src/app/shared/models/cart';
 import { CartService } from 'src/app/services/cart.service';
 import { CartItem } from 'src/app/shared/models/cart-item';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart-page',
@@ -13,9 +13,14 @@ import { Router } from '@angular/router';
 export class CartPageComponent {
   public cart!: Cart;
 
+  public readonly minimumQuantity = 1;
+  public readonly maximumQuantity = 10;
+
+  public faCaretLeft = faCaretLeft;
+  public faCaretRight = faCaretRight;
+
   constructor(
     private cartService: CartService,
-    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -28,11 +33,13 @@ export class CartPageComponent {
     this.cartService.removeFoodFromCart(cartItem.food.id);
   }
 
-  public changeQuantity(cartItem: CartItem, quantity: number): void {
-    this.cartService.changeQuantity(cartItem.food.id, quantity);
+  public increaseQuantity(cartItem: CartItem): void {
+    if (cartItem.quantity === this.maximumQuantity) return;
+    this.cartService.changeQuantity(cartItem.food.id, cartItem.quantity + 1);
   }
 
-  public convertToNumber(value: string): number {
-    return parseInt(value);
+  public decreaseQuantity(cartItem: CartItem): void {
+    if (cartItem.quantity === this.minimumQuantity) return;
+    this.cartService.changeQuantity(cartItem.food.id, cartItem.quantity - 1);
   }
 }
